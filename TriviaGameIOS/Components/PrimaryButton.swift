72d3c4c112bdbeeb675dd6fcc4500d28777695d3
frederikhansen <frederik.hansen@student.hogent.be>
2023-12-17 13:26:16 +0100

    Initial Commit. Welcome screen added

---
+++ TriviaGameIOS/Components/PrimaryButton.swift
@@ -0,0 +1,27 @@
+//
+//  PrimaryButton.swift
+//  TriviaGameIOS
+//
+//  Created by Frederik Hansen on 17/12/2023.
+//
+
+import SwiftUI
+
+struct PrimaryButton: View {
+    var text: String
+    var background: Color = Color("AccentColor")
+    
+    var body: some View {
+        Text(text)
+            .foregroundColor(.white)
+            .padding()
+            .padding(.horizontal)
+            .background(background)
+            .cornerRadius(25)
+            .shadow(radius: 20)
+    }
+}
+
+#Preview {
+    PrimaryButton(text: "Text")
+}
