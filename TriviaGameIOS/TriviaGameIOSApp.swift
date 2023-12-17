d8f51bff40cbbb81ed86be837583ac825952384b
frederikhansen <frederik.hansen@student.hogent.be>
2023-12-17 12:21:06 +0100

    Initial Commit

---
+++ TriviaGameIOS/TriviaGameIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameIOSApp.swift
+//  TriviaGameIOS
+//
+//  Created by Frederik Hansen on 17/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
